57b47d633f8a86598a176859a24306fb6897906d
Lesly Lopez <leslylpz32@gmail.com>
2024-04-30 13:07:54 -0500

    completed functions file

---
+++ functions/simple_functions.js
@@ -0,0 +1,29 @@
+"use strict"
+//function to display my name, favorite movie, and favorite band
+function favoriteThings() {
+
+console.log ("My name is Lesly");
+console.log ("My favorite movie is Mama Mia");
+console.log ("My favorite band is The Beetles");
+
+}
+
+function whyImHere () {
+    console.log ("I joined the Learn to Code program because I want to learn programming");
+
+}
+
+function favoritePlace (){
+    console.log ("My favorite places to visit is New York because I like how the city looks at night")
+}
+
+
+
+
+
+//calling out functions
+favoriteThings();
+
+whyImHere ();
+
+favoritePlace();
